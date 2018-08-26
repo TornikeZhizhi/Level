098b0699e4d875333e905f1105083ac6c3df21a4
ezreal <tornike.zhizhiashvili@geolab.edu.ge>
2018-08-27 00:09:56 +0400

    no message

--- js/mine.js
+++ js/mine.js
@@ -63,8 +63,8 @@ $(window).on('load', function (){
 
         $(document).ready(function() {
               $('.owl-carousel').owlCarousel({
-              	animateIn: 'fadeIn',
-	            animateOut: 'fadeOut',
+             //  	animateIn: 'fadeIn',
+	            // animateOut: 'fadeOut',
                 dotsSpeed: 1000,
 	            dragEndSpeed: 1000,
               	loop:true,
@@ -96,8 +96,161 @@ $(document).ready(function(){
 	
 
 	})
-
-
 })      
+
+
+// Scroll Animations
+
+$(document).ready(function(){
+	$(window).on("load",function(){
+		 if($(window).width() > 992){
+
+		 	$(window).on('scroll', check_if_in_view);
+$(window).on('scroll resize', check_if_in_view);
+$(window).trigger('scroll');
+
+function check_if_in_view() {
+  var window_height = $(window).height();
+  var window_top_position = $(window).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($('.idea_tittle'), function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+
+      $('.idea_tittle').addClass('bounceInLeft');
+      $('.idea_text').addClass('bounceInRight');
+
+    }
+
+  });
+}
+
+$(window).on('scroll', check_if_in_view2);
+$(window).on('scroll resize', check_if_in_view2);
+$(window).trigger('scroll');
+
+function check_if_in_view2() {
+  var window_height = $(window).height();
+  var window_top_position = $(window).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($('.idea_scroll1'), function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+   
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+
+      $('.idea_scroll1').addClass('bounceInLeft');
+      $('.idea_scroll2').addClass('bounceInRight');
+
+    }
+
+  });
+}
+
+$(window).on('scroll', check_if_in_view3);
+$(window).on('scroll resize', check_if_in_view3);
+$(window).trigger('scroll');
+
+function check_if_in_view3() {
+  var window_height = $(window).height();
+  var window_top_position = $(window).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($('.whowe_img_box'), function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+
+      $('.whowe_img_box').addClass('bounceInLeft');
+      $('.whowe_main_info').addClass('bounceInRight');
+      $('.info_ypc').addClass('bounceInRight');
+
+    }
+
+  });
+}
+
+
+
+$(window).on('scroll', check_if_in_view4);
+$(window).on('scroll resize', check_if_in_view4);
+$(window).trigger('scroll');
+
+function check_if_in_view4() {
+  var window_height = $(window).height();
+  var window_top_position = $(window).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($('.projects_img_left'), function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+  
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+
+      $('.projects_img_left').addClass('bounceInLeft');
+      $('.projects_img_right').addClass('bounceInRight');
+     
+
+    }
+
+  });
+}
+
+
+
+$(window).on('scroll', check_if_in_view5);
+$(window).on('scroll resize', check_if_in_view5);
+$(window).trigger('scroll');
+
+function check_if_in_view5() {
+  var window_height = $(window).height();
+  var window_top_position = $(window).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($('.aboutus_box'), function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+ 
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+
+      $('.aboutus_box').addClass('bounceInLeft');
+     
+      $('.footer_map').addClass('bounceInUp');
+      $('.footer_nav_box').addClass('bounceInRight');
+
+    }
+
+  });
+}
+
+		 }
+	})
+})
+
 
 
